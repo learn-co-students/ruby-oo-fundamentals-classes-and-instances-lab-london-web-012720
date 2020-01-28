84573491328ae876dd78f75690e381105851e492
github username <github email address>
2020-01-28 13:45:04 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,11 @@
+# Open lib/person.rb and add a class definition for a Person class.
+
+# Under your Person class definition, create two people in local variables,
+# adele_goldberg and alan_kay
+
+class Person
+
+end
+
+adele_goldberg = Person.new
+alan_kay = Person.new
